fa4be8bf07028e4662525291f3ff92a8c1f78d88
Oluwayetty <oluwayetti@gmail.com>
2018-11-06 23:39:26 +0100

    playground with RFC

--- scripts/lg.py
+++ scripts/lg.py
@@ -5,6 +5,9 @@ import pandas as pd
 import pdb
 from sklearn import cross_validation
 from sklearn.linear_model import LogisticRegression as LG
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn import tree
+from sklearn.naive_bayes import BernoulliNB as NB
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score as acc
@@ -22,7 +25,8 @@ output_integer_encoded  = label_encoder.fit_transform(training_data['output'])
 features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(training_data.iloc[:,2:10], output_integer_encoded, test_size=0.3, random_state=0)
 
 # train our model with LogisticRegression
-clf = LG().fit(features_train, labels_train)
+# tree.DecisionTreeClassifier().fit()
+clf = RFC().fit(features_train, labels_train)
 
 # use our model to predict
 predictions_class_test = clf.predict(features_test)
